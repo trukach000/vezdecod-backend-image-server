a356ac93ca61d86053ed3fa2a6f56f89cc834ab8
root <root@5-63-157-208.cloudvps.regruhosting.ru>
2021-11-05 13:24:12 +0300

    added redis host for docker

--- docker-start.sh
+++ docker-start.sh
@@ -1,3 +1,3 @@
 docker build -t imloader .  
 
-docker run -dp 30001:80 -e GLOBAL_PREFIX=/imloader -e DB_HOST=host.docker.internal --add-host host.docker.internal:host-gateway  imloader 
+docker run -dp 30001:80 -e GLOBAL_PREFIX=/imloader -e REDIS_HOST=host.docker.internal -e DB_HOST=host.docker.internal --add-host host.docker.internal:host-gateway  imloader 
