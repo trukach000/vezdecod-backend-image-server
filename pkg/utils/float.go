9f96f6875fe6d464dc6a6d6c472f1f3fd42b078f
Pavel <trukach000@gmail.com>
2021-11-05 10:00:59 +0300

    small fix

--- pkg/utils/float.go
+++ pkg/utils/float.go
@@ -7,3 +7,7 @@ const float64EqualityThreshold = 1e-9
 func AlmostEqual(a, b float64) bool {
 	return math.Abs(a-b) <= float64EqualityThreshold
 }
+
+func EqualWithPrecision(a, b, precision float64) bool {
+	return math.Abs(a-b) <= precision
+}
